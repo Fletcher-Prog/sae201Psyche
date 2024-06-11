8de842b27a78fdabfa18710a7f213c70b61857dc
Fletcher-Prog <121747499+Fletcher-Prog@users.noreply.github.com>
2024-06-11 10:42:57 +0200

    Fix

--- routes/Controleur.java
+++ routes/Controleur.java
@@ -1,12 +1,45 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Controleur 
 {
 	private Plateau j1;
 	private Plateau j2;
+	private List tabMine;
 
 	public Controleur()
 	{
-		this.j1= new Plateau ();
-		this.j2= new Plateau ();
+		this.j1      = new Plateau ();
+		this.j2      = new Plateau ();
+		this.tabMine = new ArrayList<Mine>();
+	}
+
+
+	public void initMine()
+	{
+		String tmpCoul = "";
+		int    tmpZone = -1;
+
+		String[] tabMine = {"J1","J5","J2","J3","J4","B2","B3","B4","B6","B8","G0","G1","G2","G3","G4","V2","V3","V4","V6","V8","R1","R2","R3","R4","R5","M1","M2","M3","M4","M5"};
+
+		for ( String mine : tabMine )
+		{
+			switch ( mine.substring( 0, 0 ) )
+			{
+				case "J" -> tmpCoul = "Jaune";
+				case "B" -> tmpCoul = "Bleu";
+				case "G" -> tmpCoul = "Gris";
+				case "V" -> tmpCoul = "Vert";
+				case "R" -> tmpCoul = "Rouge";
+				case "M" -> tmpCoul = "Marron";
+			}
+
+			tmpZone = Integer.parseInt( mine.substring( 1, 1 ) );
+
+			this.tabMine.add( new Mine( tmpZone, tmpCoul ) );
+ 
+		}
+	
 	}
 
 	public static void main (String[] arg)
---
+++ routes/metier/Mine.java
@@ -0,0 +1,18 @@
+public class Mine
+{
+
+    private int    numMine;
+    private String nomCoul;
+    
+    private Mine( int numMine, String nomCoul )
+    {
+        this.numMine = numMine;
+        this.nomCoul = nomCoul;
+    }
+
+    public int    getNumMine() { return this.numMine; }
+    public String getNomCoul() { return this.nomCoul; }
+
+    
+
+}
